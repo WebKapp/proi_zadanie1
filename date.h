3ef62412beda96b54c51698a6d3077553d18c322
Kacper <kacper.mur123@gmail.com>
2022-03-09 08:47:07 +0100

    created class Date in date.h

--- date.h
+++ date.h
@@ -1,13 +1,29 @@
-//
-// Created by Kacper Murygin on 08/03/2022.
-//
-
 #ifndef ZADANIE1_DATE_H
 #define ZADANIE1_DATE_H
 
 
-class date {
+class Date {
+private:
+    int day;
+    int month;
+    int year;
 
+    bool checkDate(int d, int m, int y);
+
+public:
+    Date(int d, int m, int y);
+
+    void setDay(int d);
+
+    void setMonth(int m);
+
+    void setYear(int y);
+
+    int getDay();
+
+    int getMonth();
+
+    int getYear();
 };
 
 
