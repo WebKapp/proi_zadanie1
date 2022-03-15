090d775450646a055744a6e1c826e395c650e685
Kacper <kacper.mur123@gmail.com>
2022-03-15 11:33:21 +0100

    Initial Commit

---
+++ zadanie1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  zadanie1
+//
+//  Created by Kacper Murygin on 15/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
